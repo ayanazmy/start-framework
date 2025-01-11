1a27cf6d7cade5a29b7a41e3fbd6db5bc7cc6933
ayanazmy <ayanazmy.work@gmail.com>
2025-01-11 21:12:45 +0200

    fixed modal size and bg-color

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -4,8 +4,8 @@ const Modal = ({ img, handleCloseModal }) => {
     return (
         <>
             <div className="modal position-absolute top-0 bottom-0 start-0 end-0 d-flex justify-content-center align-items-center" onClick={(e) => handleCloseModal(e)}>
-                <div className="image-container w-50 mx-auto">
-                    <img src={img} alt="" className='w-100 h-50' />
+                <div className="image-container w-50 h-100 mx-auto d-flex justify-content-center align-items-center">
+                    <img src={img} alt="" className='w-75 h-50' />
                 </div>
             </div>
         </>
